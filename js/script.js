575989666c57ffd71c4242cb6122e5e02424b6a5
programmercloud <88435015+programmercloud@users.noreply.github.com>
2022-07-01 17:49:48 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.clear();
-
 const app = (() => {
   let body;
   let menu;
@@ -8,19 +6,13 @@ const app = (() => {
   const init = () => {
     body = document.querySelector("body");
     menu = document.querySelector(".menu-icon");
-    menuItems = document.querySelectorAll(".nav__list-item");
+    menuItems = document.querySelector(".nav__list-item");
 
     applyListeners();
   };
 
-  const applyListeners = () => {
-    menu.addEventListener("click", () => toggleClass(body, "nav-active"));
-  };
-
-  const toggleClass = (element, stringClass) => {
-    if (element.classList.contains(stringClass))
-      element.classList.remove(stringClass);
-    else element.classList.add(stringClass);
+  const applyListeners = () =>{
+    menu.addEventListener('click', () => body.classList.toggle('nav-active'));
   };
 
   init();
